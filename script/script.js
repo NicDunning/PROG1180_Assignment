b1565c6bdb5e4492d35410c94200e2a9aa8bbc53
NicDunning <72279040+NicDunning@users.noreply.github.com>
2022-10-10 23:17:07 -0400

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,25 +1,48 @@
 // $(document).ready(function() {
 
-//     $('#tbInventory tr').click(function() {
-//         var href = $(this).find("a").attr("href");
-//         if(href) {
-//             window.location = href;
-//         }
-//     });
+//     // $('#tbInventory tr').click(function() {
+//     //     var href = $(this).find("a").attr("href");
+//     //     if(href) {
+//     //         window.location = href;
+//     //     }
+//     // });
 
+//     RefreshDisplayedData;
 // });
 
-$(document).ready(RefreshDisplayedData());
 
-const tbInventory = document.GetElementById("tbInventory");
+document.onreadystatechange = function () {
+    if (document.readyState == "interactive") {
+        RefreshDisplayedData();
+    }
+}
+
+
 
 function RefreshDisplayedData(){
-    tbInventory.InnerHTML = "
-            <tr>
-                <th>Edit</th>
-                <th>UPC</th>
-                <th>Product</th>
-                <th>Quantity</th>
-                <th>Price</th>
-            </tr>"
+    // Init Variables
+    const tblInventory = document.getElementById("tbInventory");
+    var tblInventoryHeaders = ["Edit", "UPC", "Product", "Details", "Quantity", "Price"];
+    var tblInventoryHTML = "<tr>";
+    // Foreach value in headers make a column.
+    tblInventoryHeaders.forEach(header => {
+        tblInventoryHTML += `<th>${header}</th>`;
+    });
+    // Set innerHTML add table header.
+    tblInventory.innerHTML += tblInventoryHTML + "</tr>";
+    // Foreach key/ value pair in items dictionary[0]
+    var itemRow = "<tr><td><a href='inventory.html'>Edit</a></td>";
+    for (const [key, value] of Object.entries(items[0])){
+        // If it is a displayed value.
+        if(!(["Status", "ManFactID", "SerialNum"].includes(key))){
+            // Add it to the row.
+            itemRow += `<td>${value}</td>`;
+        }
+    }
+    itemRow += "</tr>";
+    // Add the row to the table.
+    tblInventory.innerHTML += itemRow; 
+
+
+    
 }
