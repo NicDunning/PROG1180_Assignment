738352e90f5f007cd8a305cd1307a1da066525cd
NicDunning <72279040+NicDunning@users.noreply.github.com>
2022-10-10 21:58:28 -0400

    Update script.js
    Scripts Test 1

--- script/script.js
+++ script/script.js
@@ -1,10 +1,25 @@
-$(document).ready(function() {
+// $(document).ready(function() {
 
-    $('#tbInventory tr').click(function() {
-        var href = $(this).find("a").attr("href");
-        if(href) {
-            window.location = href;
-        }
-    });
+//     $('#tbInventory tr').click(function() {
+//         var href = $(this).find("a").attr("href");
+//         if(href) {
+//             window.location = href;
+//         }
+//     });
 
-});
+// });
+
+$(document).ready(RefreshDisplayedData());
+
+const tbInventory = document.GetElementById("tbInventory");
+
+function RefreshDisplayedData(){
+    tbInventory.InnerHTML = "
+            <tr>
+                <th>Edit</th>
+                <th>UPC</th>
+                <th>Product</th>
+                <th>Quantity</th>
+                <th>Price</th>
+            </tr>"
+}
