35585596db44f24ceff0c397ed8be15837ed81fc
EsperMaudlin <62960191+EsperMaudlin@users.noreply.github.com>
2022-10-23 21:15:09 -0500

    Update INVSYS_Validation_v1.0.js
    Fixed a couple silly errors and adjusted some variable types to be "more correct"

--- script/INVSYS_Validation_v1.0.js
+++ script/INVSYS_Validation_v1.0.js
@@ -1,9 +1,9 @@
 /***********************
 * INVENTORY VALIDATION *
-************************/
+***********************/
 function validateUPC(UPC) {
-    let regex = "^\\d{12}$"
-    let string = UPC.ToString().split("-").join("").split(" ").join("")
+    const regex = "^\\d{12}$"
+    const string = UPC.ToString().split("-").join("").split(" ").join("")
 
     if (!string.match(regex)) {
         return "Invalid UPC. Please provide a 12 digit number."
@@ -15,7 +15,7 @@ function validateQuantity(qty) {
     if (!Number.isInteger(+qty)) {
         return "Invalid quantity. Please provide a positive integer value."
     }
-    if (qty < 0) {
+    if (+qty < 0) {
         return "Invalid quantity. Please provide a positive integer value."
     }
     return
@@ -38,16 +38,17 @@ function validateDate(input) {
 }
 
 function validateCost(cost) {
+    const regex = "^\\d*\\.\\d*\\.\\d*$"
     let string = cost.ToString()
     
     if (Number.isNaN(+cost)) {
         return "Invalid cost. Please provide a positive numerical value."
     }
-    if (qty < 0) {
+    if (+cost < 0) {
         return "Invalid cost. Please provide a positive value."
     }
     if (string.includes(".")) {
-        if (string.split(".")[1].length > 2) {
+        if ((string.split(".")[1].length > 2) || (string.match(regex))) {
             return "Invalid cost. Please provide a positive value with no more than two digits after the decimal point."
         }   
     }
@@ -58,10 +59,10 @@ function validateCost(cost) {
 
 /*********************
 * PRODUCT VALIDATION *
-**********************/
+*********************/
 function validateUPC(UPC) {
-    let regex = "^\\d{12}$"
-    let string = UPC.ToString().split("-").join("").split(" ").join("")
+    const regex = "^\\d{12}$"
+    const string = UPC.ToString().split("-").join("").split(" ").join("")
 
     if (!string.match(regex)) {
         return "Invalid UPC. Please provide a 12 digit number."
@@ -84,10 +85,10 @@ function validateDetails(string) {
 }
 
 function validateSerial(string) {
-    let regex = "^[A-Za-z0-9]+$"
+    const regex = "^[A-Za-z0-9]+$"
     
     if (!string.match(regex)) {
-        return "Invalid Serial Number. Please provide a serial number containing only letters and/or numbers."
+        return "Invalid serial number. Please provide a serial number containing only letters and/or numbers."
     }
     return
 }
@@ -96,7 +97,7 @@ function validateSerial(string) {
 
 /**********************
 * SUPPLIER VALIDATION *
-***********************/
+**********************/
 function validateName(string) {
     if (string.length > 30)  {
         return "Invalid name. Please provide a name of length 30 characters or less."
@@ -106,38 +107,38 @@ function validateName(string) {
 
 function validateStreet(string) {
     if (string.length > 50) {
-        return "Invalid Street Address. Please provide a street address of length 50 characters or less."
+        return "Invalid street address. Please provide a street address of length 50 characters or less."
     }
     return
 }
 
 function validateCity(string) {
     if (string.length > 80) {
-        return "Invalid City Name. Please provide a city name of length 80 characters or less."
+        return "Invalid city name. Please provide a city name of length 80 characters or less."
     }
     return
 }
 
 function validateProvince(string) {
-    provinces = [AB, BC, MB, NB, NL, NS, NT, NU, ON, PE, QC, SK, YE]
+    const provinces = [AB, BC, MB, NB, NL, NS, NT, NU, ON, PE, QC, SK, YE]
     if (!provinces.includes(string.ToUpper())) {
-        return "Invalid Province Code. Please provide a valid two-letter province code."
+        return "Invalid province code. Please provide a valid two-letter province code."
     }
     return
 }
 
 function validatePostCode(string) {
-    let regex = "^[A-Za-z]\\d[A-Za-z][ \-]?\\d[A-Za-z]\\d$"
+    const regex = "^[A-Za-z]\\d[A-Za-z][ \-]?\\d[A-Za-z]\\d$"
     if (!string.match(regex)) {
-        return "Invalid Post Code. Please provide a valid post code. Example: \"L3C 7L3\""
+        return "Invalid post code. Please provide a valid post code. Example: \"L3C 7L3\""
     }
     return
 }
 
 function validatePhone(string) {
-    let regex = "^\\s*\\+?\\d{0,2}[. -]?\\(?\\d{3}\\)?[. -]?\\d{3}[. -]?\\d{4}[. -]?x?(\\d{4})?"
+    const regex = "^\\s*\\+?\\d{0,2}[. -]?\\(?\\d{3}\\)?[. -]?\\d{3}[. -]?\\d{4}[. -]?x?(\\d{4})?\\s*$"
     if (!string.match(regex)) {
-        return "Invalid Phone Number. Please provide a valid phone number."
+        return "Invalid phone number. Please provide a valid phone number."
     }
     return
 }
@@ -146,57 +147,57 @@ function validatePhone(string) {
 
 /**********************
 * CUSTOMER VALIDATION *
-***********************/
+**********************/
 function validateFirstName(string) {
-    let regex = "^[A-Za-zÀ-ÖØ-öø-ÿ -.']{1,50}$"
+    const regex = "^[A-Za-zÀ-ÖØ-öø-ÿ -.']{1,50}$"
     if (!string.match(regex)) {
-        return "Invalid First Name. Please provide a first name of length 50 characters or shorter using only letters, spaces, or the following characters: - . '"
+        return "Invalid first name. Please provide a first name of length 50 characters or shorter using only letters, spaces, or the following characters: - . '"
     }
     return
 }
 
 function validateLastName(string) {
-    let regex = "^[A-Za-zÀ-ÖØ-öø-ÿ -.']{1,100}$"
+    const regex = "^[A-Za-zÀ-ÖØ-öø-ÿ -.']{1,100}$"
     if (!string.match(regex)) {
-        return "Invalid Last Name. Please provide a last name of length 100 characters or shorter using only letters, spaces, or the following characters: - . '"
+        return "Invalid last name. Please provide a last name of length 100 characters or shorter using only letters, spaces, or the following characters: - . '"
     }
     return
 }
 
 function validateStreet(string) {
     if (string.length > 50) {
-        return "Invalid Street Address. Please provide a street address of length 50 characters or less."
+        return "Invalid street address. Please provide a street address of length 50 characters or less."
     }
     return
 }
 
 function validateCity(string) {
     if (string.length > 80) {
-        return "Invalid City Name. Please provide a city name of length 80 characters or less."
+        return "Invalid city name. Please provide a city name of length 80 characters or less."
     }
     return
 }
 
 function validateProvince(string) {
-    provinces = [AB, BC, MB, NB, NL, NS, NT, NU, ON, PE, QC, SK, YE]
+    const provinces = [AB, BC, MB, NB, NL, NS, NT, NU, ON, PE, QC, SK, YE]
     if (!provinces.includes(string.ToUpper())) {
-        return "Invalid Province Code. Please provide a valid two-letter province code."
+        return "Invalid province code. Please provide a valid two-letter province code."
     }
     return
 }
 
 function validatePostCode(string) {
-    let regex = "^[A-Za-z]\\d[A-Za-z][ \-]?\\d[A-Za-z]\\d$"
+    const regex = "^[A-Za-z]\\d[A-Za-z][ \-]?\\d[A-Za-z]\\d$"
     if (!string.match(regex)) {
-        return "Invalid Post Code. Please provide a valid post code. Example: \"L3C 7L3\""
+        return "Invalid post code. Please provide a valid post code. Example: \"L3C 7L3\""
     }
     return
 }
 
 function validatePhone(string) {
-    let regex = "^\\s*\\+?\\d{0,2}[. -]?\\(?\\d{3}\\)?[. -]?\\d{3}[. -]?\\d{4}[. -]?x?(\\d{4})?"
+    const regex = "^\\s*\\+?\\d{0,2}[. -]?\\(?\\d{3}\\)?[. -]?\\d{3}[. -]?\\d{4}[. -]?x?(\\d{4})?"
     if (!string.match(regex)) {
-        return "Invalid Phone Number. Please provide a valid phone number."
+        return "Invalid phone number. Please provide a valid phone number."
     }
     return
 }
@@ -228,25 +229,25 @@ function validateDate(input) {
  * EMPLOYEE VALIDATION *
  **********************/
 function validateFirstName(string) {
-    let regex = "^[A-Za-zÀ-ÖØ-öø-ÿ -.']{1,50}$"
+    const regex = "^[A-Za-zÀ-ÖØ-öø-ÿ -.']{1,50}$"
     if (!string.match(regex)) {
-        return "Invalid First Name. Please provide a first name of length 50 characters or shorter using only letters, spaces, or the following characters: - . '"
+        return "Invalid first name. Please provide a first name of length 50 characters or shorter using only letters, spaces, or the following characters: - . '"
     }
     return
 }
 
 function validateLastName(string) {
-    let regex = "^[A-Za-zÀ-ÖØ-öø-ÿ -.']{1,100}$"
+    const regex = "^[A-Za-zÀ-ÖØ-öø-ÿ -.']{1,100}$"
     if (!string.match(regex)) {
-        return "Invalid Last Name. Please provide a last name of length 100 characters or shorter using only letters, spaces, or the following characters: - . '"
+        return "Invalid last name. Please provide a last name of length 100 characters or shorter using only letters, spaces, or the following characters: - . '"
     }
     return
 }
 
 function validateDeptName(string) {
-    let regex="^[A-Za-z0-9]{4}$"
+    const regex="^[A-Za-z0-9]{4}$"
     if (!string.match(regex)) {
-        return "Invalid Department Name. Please provide a department name four characters in length using only letters and/or numbers."
+        return "Invalid department name. Please provide a department name four characters in length using only letters and/or numbers."
     }
     return
 }
