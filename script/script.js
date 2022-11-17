07e8f4f20449bc1f0981e4f0bf207130732962ee
NicDunning <72279040+NicDunning@users.noreply.github.com>
2022-11-16 21:57:39 -0500

    Update script.js

--- script/script.js
+++ script/script.js
@@ -16,7 +16,7 @@ document.onreadystatechange = function () {
 
     if(!chklogin){
         console.log("problem");
-        window.location.replace("./login.html");
+        //window.location.replace("./login.html");
     }
 
     if (document.readyState == "interactive") {
